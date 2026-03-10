f3f79fbb5252b61778a990ec1805a77d013bc627
Alexrohjak <alexander.rohde.jakobsen@gmail.com>
2026-03-10 14:52:51 +0100

    la til alle måner i program.cs

--- Planets/Planets/Program.cs
+++ Planets/Planets/Program.cs
@@ -4,76 +4,216 @@ using SpaceLib;
 
 namespace SpaceSim
 {
-	class Astronomy
-	{
-		public static void Main(String[] args)
-		{
-			Star theSun = new Star("The sun", 696340, 609.12, 0, 0, ConsoleColor.Yellow);
+    class Astronomy
+    {
+        public static void Main(String[] args)
+        {
+            Star theSun = new Star("The sun", 696340, 609.12, 0, 0, ConsoleColor.Yellow);
 
-			Planet mercury = new Planet("Mercury", 2439.7, 1407.6, 57.9, 88, ConsoleColor.Gray);
-			Planet venus = new Planet("Venus", 6051.8, -5832.5, 108.2, 224.7, ConsoleColor.Yellow);
-			Planet earth = new Planet("Earth", 6371, 24, 149.6, 365.25, ConsoleColor.Blue);
-			Planet mars = new Planet("Mars", 3389.5, 24.6, 227.9, 687, ConsoleColor.Red);
+            Planet mercury = new Planet("Mercury", 2439.7, 1407.6, 57.9, 88, ConsoleColor.Gray);
+            Planet venus = new Planet("Venus", 6051.8, -5832.5, 108.2, 224.7, ConsoleColor.Yellow);
+            Planet earth = new Planet("Earth", 6371, 24, 149.6, 365.25, ConsoleColor.Blue);
+            Planet mars = new Planet("Mars", 3389.5, 24.6, 227.9, 687, ConsoleColor.Red);
 
-			Planet jupiter = new Planet("Jupiter", 69911, 9.9, 778.5, 4333, ConsoleColor.DarkYellow);
-			Planet saturn = new Planet("Saturn", 58232, 10.7, 1434, 10759, ConsoleColor.Yellow);
-			Planet uranus = new Planet("Uranus", 25362, -17.2, 2871, 30687, ConsoleColor.Cyan);
-			Planet neptune = new Planet("Neptune", 24622, 16.1, 4495, 60190, ConsoleColor.Blue);
+            Planet jupiter = new Planet("Jupiter", 69911, 9.9, 778.5, 4333, ConsoleColor.DarkYellow);
+            Planet saturn = new Planet("Saturn", 58232, 10.7, 1434, 10759, ConsoleColor.Yellow);
+            Planet uranus = new Planet("Uranus", 25362, -17.2, 2871, 30687, ConsoleColor.Cyan);
+            Planet neptune = new Planet("Neptune", 24622, 16.1, 4495, 60190, ConsoleColor.Blue);
 
-			Comet halley = new Comet("Halley", 11, 52.8, 2660, 27475, ConsoleColor.White);
-			Asteroid asteroid = new Asteroid("Asteroid", 262.7, 5.3, 353, 1325, ConsoleColor.DarkGray);
-			AsteroidBelt asteroidBelt = new AsteroidBelt("Asteroid Belt", 0, 0, 414, 1680, ConsoleColor.DarkGray);
-			DwarfPlanet pluto = new DwarfPlanet("Pluto", 1188.3, 153.3, 5906, 90560, ConsoleColor.Gray);
+            Comet halley = new Comet("Halley", 11, 52.8, 2660, 27475, ConsoleColor.White);
+            Asteroid asteroid = new Asteroid("Asteroid", 262.7, 5.3, 353, 1325, ConsoleColor.DarkGray);
+            AsteroidBelt asteroidBelt = new AsteroidBelt("Asteroid Belt", 0, 0, 414, 1680, ConsoleColor.DarkGray);
+            DwarfPlanet pluto = new DwarfPlanet("Pluto", 1188.3, 153.3, 5906, 90560, ConsoleColor.Gray);
 
-			//Navn, RadiusObjekt, LengdePåDag(timer), AvstandFraSentrum(km), TilForFullRundeRundtSentrum, Farge
-			Moon theMoon = new Moon("The moon", 1737.4, 655.7, 0.384, 27.3, ConsoleColor.Gray, earth);
-			earth.AddMoon(theMoon);
+            //Navn, RadiusObjekt, LengdePåDag(timer), AvstandFraSentrum(km), TilForFullRundeRundtSentrum, Farge
 
-			List<SpaceObject> solarsystem = new List<SpaceObject>() { theSun, mercury, venus, earth, theMoon, mars, jupiter, saturn, uranus, neptune, pluto, halley, asteroid, asteroidBelt };
+            // oppretter alle maanene
+            Moon theMoon = new Moon("The moon", 1737.4, 655.7, 0.384, 27.3, ConsoleColor.Gray, earth);
+            earth.AddMoon(theMoon);
+
+            // Mars
+            Moon phobos = new Moon("Phobos", 11.3, 7.7, 9, 0.32, ConsoleColor.Gray, mars);
+            Moon deimos = new Moon("Deimos", 6.2, 30.3, 23, 1.26, ConsoleColor.Gray, mars);
+
+            // Jupiter inner moons
+            Moon metis = new Moon("Metis", 10, 7, 128, 0.29, ConsoleColor.Gray, jupiter);
+            Moon adrastea = new Moon("Adrastea", 8, 7, 129, 0.30, ConsoleColor.Gray, jupiter);
+            Moon amalthea = new Moon("Amalthea", 84, 12, 181, 0.50, ConsoleColor.Gray, jupiter);
+            Moon thebe = new Moon("Thebe", 49, 12, 222, 0.67, ConsoleColor.Gray, jupiter);
+            Moon io = new Moon("Io", 1821, 42, 422, 1.77, ConsoleColor.Yellow, jupiter);
+            Moon europa = new Moon("Europa", 1560, 85, 671, 3.55, ConsoleColor.Gray, jupiter);
+            Moon ganymede = new Moon("Ganymede", 2634, 172, 1070, 7.15, ConsoleColor.Gray, jupiter);
+            Moon callisto = new Moon("Callisto", 2410, 400, 1883, 16.69, ConsoleColor.Gray, jupiter);
+
+            // Jupiter outer moons
+            Moon leda = new Moon("Leda", 8, 12, 11165, 238.72, ConsoleColor.Gray, jupiter);
+            Moon himalia = new Moon("Himalia", 93, 7.8, 11480, 250.56, ConsoleColor.Gray, jupiter);
+            Moon lysithea = new Moon("Lysithea", 18, 12, 11720, 259.22, ConsoleColor.Gray, jupiter);
+            Moon elara = new Moon("Elara", 38, 12, 11737, 259.65, ConsoleColor.Gray, jupiter);
+            Moon ananke = new Moon("Ananke", 15, 12, 21200, -631, ConsoleColor.Gray, jupiter);
+            Moon carme = new Moon("Carme", 23, 12, 22600, -692, ConsoleColor.Gray, jupiter);
+            Moon pasiphae = new Moon("Pasiphae", 30, 12, 23500, -735, ConsoleColor.Gray, jupiter);
+            Moon sinope = new Moon("Sinope", 19, 12, 23700, -758, ConsoleColor.Gray, jupiter);
+
+            // Saturn moons
+            Moon atlas = new Moon("Atlas", 15, 12, 138, 0.60, ConsoleColor.Gray, saturn);
+            Moon prometheus = new Moon("Prometheus", 43, 12, 139, 0.61, ConsoleColor.Gray, saturn);
+            Moon pandora = new Moon("Pandora", 40, 12, 142, 0.63, ConsoleColor.Gray, saturn);
+            Moon epimetheus = new Moon("Epimetheus", 65, 12, 151, 0.69, ConsoleColor.Gray, saturn);
+            Moon janus = new Moon("Janus", 89, 12, 151, 0.69, ConsoleColor.Gray, saturn);
+            Moon mimas = new Moon("Mimas", 198, 23, 186, 0.94, ConsoleColor.Gray, saturn);
+            Moon enceladus = new Moon("Enceladus", 252, 33, 238, 1.37, ConsoleColor.Gray, saturn);
+            Moon tethys = new Moon("Tethys", 531, 45, 295, 1.89, ConsoleColor.Gray, saturn);
+            Moon telesto = new Moon("Telesto", 15, 12, 295, 1.89, ConsoleColor.Gray, saturn);
+            Moon calypso = new Moon("Calypso", 13, 12, 295, 1.89, ConsoleColor.Gray, saturn);
+            Moon dione = new Moon("Dione", 561, 66, 377, 2.74, ConsoleColor.Gray, saturn);
+            Moon helene = new Moon("Helene", 18, 12, 377, 2.74, ConsoleColor.Gray, saturn);
+            Moon rhea = new Moon("Rhea", 764, 108, 527, 4.52, ConsoleColor.Gray, saturn);
+            Moon titan = new Moon("Titan", 2575, 382, 1222, 15.95, ConsoleColor.Gray, saturn);
+            Moon hyperion = new Moon("Hyperion", 143, 21, 1481, 21.28, ConsoleColor.Gray, saturn);
+            Moon iapetus = new Moon("Iapetus", 734, 1903, 3561, 79.33, ConsoleColor.Gray, saturn);
+            Moon phoebe = new Moon("Phoebe", 107, 9.3, 12952, -550.48, ConsoleColor.Gray, saturn);
+
+            // Uranus moons
+            Moon ariel = new Moon("Ariel", 581, 60, 191, 2.52, ConsoleColor.Gray, uranus);
+            Moon umbriel = new Moon("Umbriel", 585, 84, 266, 4.14, ConsoleColor.Gray, uranus);
+            Moon titania = new Moon("Titania", 789, 209, 436, 8.71, ConsoleColor.Gray, uranus);
+            Moon oberon = new Moon("Oberon", 761, 324, 583, 13.46, ConsoleColor.Gray, uranus);
+            Moon miranda = new Moon("Miranda", 236, 34, 130, 1.41, ConsoleColor.Gray, uranus);
+
+            // Neptune moons
+            Moon triton = new Moon("Triton", 1353, 141, 355, -5.88, ConsoleColor.Gray, neptune);
+            Moon nereid = new Moon("Nereid", 170, 11, 5513, 360.13, ConsoleColor.Gray, neptune);
+
+            mars.AddMoon(phobos);
+            mars.AddMoon(deimos);
+
+            jupiter.AddMoon(metis);
+            jupiter.AddMoon(adrastea);
+            jupiter.AddMoon(amalthea);
+            jupiter.AddMoon(thebe);
+            jupiter.AddMoon(io);
+            jupiter.AddMoon(europa);
+            jupiter.AddMoon(ganymede);
+            jupiter.AddMoon(callisto);
+            jupiter.AddMoon(leda);
+            jupiter.AddMoon(himalia);
+            jupiter.AddMoon(lysithea);
+            jupiter.AddMoon(elara);
+            jupiter.AddMoon(ananke);
+            jupiter.AddMoon(carme);
+            jupiter.AddMoon(pasiphae);
+            jupiter.AddMoon(sinope);
+
+            saturn.AddMoon(atlas);
+            saturn.AddMoon(prometheus);
+            saturn.AddMoon(pandora);
+            saturn.AddMoon(epimetheus);
+            saturn.AddMoon(janus);
+            saturn.AddMoon(mimas);
+            saturn.AddMoon(enceladus);
+            saturn.AddMoon(tethys);
+            saturn.AddMoon(telesto);
+            saturn.AddMoon(calypso);
+            saturn.AddMoon(dione);
+            saturn.AddMoon(helene);
+            saturn.AddMoon(rhea);
+            saturn.AddMoon(titan);
+            saturn.AddMoon(hyperion);
+            saturn.AddMoon(iapetus);
+            saturn.AddMoon(phoebe);
+
+            uranus.AddMoon(ariel);
+            uranus.AddMoon(umbriel);
+            uranus.AddMoon(titania);
+            uranus.AddMoon(oberon);
+            uranus.AddMoon(miranda);
+
+            neptune.AddMoon(triton);
+            neptune.AddMoon(nereid);
+
+            List<SpaceObject> solarsystem = new List<SpaceObject>()
+                {
+                    theSun,
+
+                    mercury,
+                    venus,
+
+                    earth, theMoon,
+
+                    mars, phobos, deimos,
+
+                    jupiter,
+                    metis, adrastea, amalthea, thebe,
+                    io, europa, ganymede, callisto,
+                    leda, himalia, lysithea, elara,
+                    ananke, carme, pasiphae, sinope,
+
+                    saturn,
+                    atlas, prometheus, pandora,
+                    epimetheus, janus,
+                    mimas, enceladus,
+                    tethys, telesto, calypso,
+                    dione, helene,
+                    rhea, titan, hyperion,
+                    iapetus, phoebe,
+
+                    uranus,
+                    miranda, ariel, umbriel, titania, oberon,
+
+                    neptune,
+                    triton, nereid,
+
+                    pluto,
+
+                    halley,
+                    asteroid,
+                    asteroidBelt
+                };
 
 
-			Console.Write("Enter amount of hours: ");
-			double time = double.Parse(Console.ReadLine());
+            Console.Write("Enter amount of hours: ");
+            double time = double.Parse(Console.ReadLine());
 
-			Console.Write("Enter name of spaceobject: ");
-			string input = Console.ReadLine();
+            Console.Write("Enter name of spaceobject: ");
+            string input = Console.ReadLine();
 
 
-			if (string.IsNullOrWhiteSpace(input))
-			{
-				//Skrive ut alle space objekter (bortsett fra månen)
-				foreach (SpaceObject obj in solarsystem)
-				{
-					if (obj is Star || obj is Planet || obj is Comet || obj is Asteroid || obj is AsteroidBelt || obj is DwarfPlanet)
-					{
-						obj.Draw();
-						var (x, y, degrees) = obj.CalculatePos(time);
-						Console.WriteLine($"Position at time={time}: , X={x:F2}, Y={y:F2}, Angle={degrees:F2}°");
-						Console.WriteLine();
-					}
-					Console.ReadLine();
-				}
-			}
-			else
-			{
-				{
-					//Kunne skrive input med små bokstaver
-					SpaceObject selectedObject = null;
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                //Skrive ut alle space objekter (bortsett fra månen)
+                foreach (SpaceObject obj in solarsystem)
+                {
+                    if (obj is Star || obj is Planet || obj is Comet || obj is Asteroid || obj is AsteroidBelt || obj is DwarfPlanet)
+                    {
+                        obj.Draw();
+                        var (x, y, degrees) = obj.CalculatePos(time);
+                        Console.WriteLine($"Position at time={time}: , X={x:F2}, Y={y:F2}, Angle={degrees:F2}°");
+                        Console.WriteLine();
+                    }
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                {
+                    //Kunne skrive input med små bokstaver
+                    SpaceObject selectedObject = null;
 
-					foreach (SpaceObject obj in solarsystem)
-					{
-						if (obj.Name.ToLower() == input.ToLower())
-						{
-							selectedObject = obj;
-							break;
-						}
-					}
-					if (selectedObject != null)
-					{
-						selectedObject.Draw();
+                    foreach (SpaceObject obj in solarsystem)
+                    {
+                        if (obj.Name.ToLower() == input.ToLower())
+                        {
+                            selectedObject = obj;
+                            break;
+                        }
+                    }
+                    if (selectedObject != null)
+                    {
+                        selectedObject.Draw();
 
-						var (x, y, degrees) = selectedObject.CalculatePos(time);
-						Console.WriteLine($"Position: X={x:F2}, Y={y:F2}, Angle={degrees:F2}");
+                        var (x, y, degrees) = selectedObject.CalculatePos(time);
+                        Console.WriteLine($"Position: X={x:F2}, Y={y:F2}, Angle={degrees:F2}");
 
                         if (selectedObject is Planet selectedPlanet)
                         {
@@ -88,26 +228,26 @@ namespace SpaceSim
                             }
                         }
                     }
-					else
-					{
-						//Hvis det er invalid input, skriver vi ut info om solen
-						Console.WriteLine("Object not found. Showing the Sun instead");
-							foreach (SpaceObject obj in solarsystem)
-							{
-								if (obj is Star)
-								{
-									obj.Draw();
+                    else
+                    {
+                        //Hvis det er invalid input, skriver vi ut info om solen
+                        Console.WriteLine("Object not found. Showing the Sun instead");
+                        foreach (SpaceObject obj in solarsystem)
+                        {
+                            if (obj is Star)
+                            {
+                                obj.Draw();
 
-									var (x, y, degrees) = obj.CalculatePos(time);
-									Console.WriteLine($"Position at time={time}: X={x:F2}, Y={y:F2}, Angle={degrees:F2}°");
-									Console.WriteLine();
-								}
-							}
-						}
+                                var (x, y, degrees) = obj.CalculatePos(time);
+                                Console.WriteLine($"Position at time={time}: X={x:F2}, Y={y:F2}, Angle={degrees:F2}°");
+                                Console.WriteLine();
+                            }
+                        }
+                    }
 
-					}
+                }
 
-				}
-			}
-		}
-	} 
+            }
+        }
+    }
+}
