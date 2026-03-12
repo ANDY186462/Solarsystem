0abd4022c06c04eff0a543a043fb95e75830b71d
Alexrohjak <alexander.rohde.jakobsen@gmail.com>
2026-03-12 13:19:35 +0100

    Update Form1.cs

--- Planets/PlanetGUI/Form1.cs
+++ Planets/PlanetGUI/Form1.cs
@@ -1,10 +1,17 @@
+using SpaceLib;
+
 namespace PlanetGUI
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private Planet selectedPlanet;
+
+        public Form1(Planet planet)
         {
             InitializeComponent();
+            selectedPlanet = planet;
+            this.DoubleBuffered = true;
+            this.ResizeRedraw = true;
         }
     }
 }
